17abb747a9f19b4734183d09fe0672331f688e3b
czij <czijnb@gmail.com>
2025-09-02 19:54:46 -0400

    Added Hello World text

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Zijun Chne
+                <p>Hello World</p>
+                UD CISC275 with React Hooks and TypeScript Zijun Chen
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
